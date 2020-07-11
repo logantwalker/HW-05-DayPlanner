0d847a72794b49815f3daacea897b58025b97631
L.Walker <logantwalker95@gmail.com>
2020-07-11 14:25:18 -0400

    day view feature - populateHours done

--- src/dayView.js
+++ src/dayView.js
@@ -3,18 +3,40 @@ const dayArr = ['Sun', 'Mon','Tue','Wed','Thur','Fri','Sat'];
 export function dayLoader(){
     let curDate = moment();
     dayTopBar(curDate);
-    populateHours(curDate);
+    populateHours();
 }
 
 function dayTopBar(curDate){
-    const hourBlocks = $('#hour-blocks');
+    const dateHead = $("#date-header");
+
     let dateValue = moment(curDate).format('DD')
     let dayValue = moment(curDate).day();
     let day = dayArr[dayValue];
-    console.log(day);
+
+    let dayEl = `<div id='dayEl' class='row'>${day}</div>`;
+    let dateEl = `<div id='dateEl' class='row'>${dateValue}</div>`;
+
+    dateHead.append(dayEl);
+    dateHead.append(dateEl);
 }
 
-function populateHours(curDate){
-    
-    
+function populateHours(){
+    const hourBlocks = $('#hour-blocks');
+    let hour;
+    for(let i = 0; i < 24 ; i++){
+       if(i===0){
+           hour='12 AM';
+       }
+       if(i!==0 && i < 12){
+           hour = `${i} AM`;
+       }
+       else if(i===12){
+        hour = `12 PM`;
+        }
+       else if(i!==0 && i > 12){
+           hour = `${i-12} PM`;
+        }
+        let hourEl=`<div id='hourEl' class='row' data-hourVal=${i}>${hour}</div>`;
+        hourBlocks.append(hourEl);
+    }
 }
